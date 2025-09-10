dea6035911f8178686fd59f01a752d8f52501fdd
Lavanya Marisetty <yagjnasri97@gmail.com>
2025-09-10 20:14:02 +0530

    ipresticted1.0

--- main.tf
+++ main.tf
@@ -16,43 +16,45 @@ provider "azurerm" {
     client_secret = var.client_secret
     tenant_id = var.tenant_id
 }
-data "azurerm_resource_group" "AZrg101" {
-    name = "AZrg1011"
+resource "azurerm_resource_group" "AZrg103" {
+    name = "AZrg1031"
+    location = "Central US"
 }
 # ✅ App Service Plan
-resource "azurerm_service_plan" "plan" {
-  name                = "internal-app-plan"
-  resource_group_name = data.azurerm_resource_group.AZrg101.name
-  location            = data.azurerm_resource_group.AZrg101.location
-  os_type             = "Windows"
-  sku_name            = "F1"
+resource "azurerm_service_plan" "plan101" {
+    name                = "internal-app-plan"
+    resource_group_name = azurerm_resource_group.AZrg103.name
+    location            = azurerm_resource_group.AZrg103.location
+    os_type             = "Windows"
+    sku_name            = "S1"
+      tags = {
+        "name" = "SP1031"
+    }
 }
 
-# ✅ Web App (Windows)
+# ✅ Web App (Windows) with IP restriction
 resource "azurerm_windows_web_app" "WebApp101" {
   name                = "internal-webapp-demo"
-  resource_group_name = data.azurerm_resource_group.AZrg101.name
-  location            = data.azurerm_resource_group.AZrg101.location
-  service_plan_id     = azurerm_service_plan.plan.id
-
+  resource_group_name = azurerm_resource_group.AZrg103.name
+  location            = azurerm_resource_group.AZrg103.location
+  service_plan_id     = azurerm_service_plan.plan101.id
+  depends_on = [ azurerm_service_plan.plan101 ]
   site_config {
     minimum_tls_version = "1.2"
-  }
+    # IP Restriction
+    ip_restriction {
+      ip_address = "106.222.228.233/32" # Replace with your VPN/VM public IP
+      action     = "Allow"
+      priority   = 100
+      name       = "VPN_Only"
+    }
 
+    ip_restriction {
+      ip_address = "0.0.0.0/0"
+      action     = "Deny"
+      priority   = 200
+      name       = "DenyAll"
+    }
+  }
   https_only = true
-}
-
-# ✅ Private Endpoint (restrict Web App inside VNet only)
-resource "azurerm_private_endpoint" "pe101" {
-  name                = "webapp-private-endpoint"
-  location            = data.azurerm_resource_group.AZrg101.location
-  resource_group_name = data.azurerm_resource_group.AZrg101.name
-  subnet_id           = var.XYZ_subnet_id # existing subnet id to fetch from keyvault
-
-  private_service_connection {
-    name                           = "webapp-privateservice"
-    private_connection_resource_id = azurerm_windows_web_app.WebApp101.id
-    subresource_names              = ["sites"]
-    is_manual_connection           = false
-  }
 }
